d4575d176d45c010881898e787e561b443616539
Your Name <you@example.com>
2026-07-06 13:04:57 +0300

    Fix syntax error in base_succession_plans_current.sql when critical_titles_sql variable is empty

--- dbt_analytics/models/marts/base_succession_plans_current.sql
+++ dbt_analytics/models/marts/base_succession_plans_current.sql
@@ -2,4 +2,8 @@
 
 SELECT s.*
     FROM {{ ref('base_succession_source_records') }} s
-    WHERE (s.is_critical = TRUE OR s.role_title IN ({{ var('critical_titles_sql') }}))
+    WHERE s.is_critical = TRUE
+    {% if var('critical_titles_sql') | trim != '' %}
+       OR s.role_title IN ({{ var('critical_titles_sql') }})
+    {% endif %}
+
